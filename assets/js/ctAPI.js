5d3c7eb60e9496c97110e26af1a677cdedbc5dea
jeoldmixon <jeoldmixon@yahoo.com>
2020-07-02 21:24:18 -0500

    js ctAPI done

---
+++ assets/js/ctAPI.js
@@ -0,0 +1,47 @@
+// ID Search box
+var searchInput = document.getElementById("searchtext") //This will need to be modified to match html page
+    // ID Ingredients
+var ingredients = document.getElementById("ingredients") //This will need to be modified to match html page
+    // ID Trivia
+    // var trivia = document.getElementById("trivia") //This will need to be modified to match html page
+    // ID Drink Pic
+var drinkPic = document.getElementById("drinkPic") //This will need to be modified to match html page
+    // ID Directions
+var directions = document.getElementById("directions") //This will need to be modified to match html page
+
+
+// https://www.thecocktaildb.com/api/json/v1/1/search.php?s=  + "var drink"
+
+$(document).ready(function() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+            directions.innerHTML = data.drinks[0].strInstructions
+            drinkPic.src = data.drinks[0].strDrinkThumb
+            var ingredientsMap = [],
+                measureMap = [],
+                finalIngredients = {}
+
+            Object.entries(data.drinks[0]).forEach(([key, val]) => {
+                console.log(val)
+                if (val) {
+                    if (key.startsWith("strIng")) ingredientsMap.push(val)
+                    else if (key.startsWith("strMea")) measureMap.push(val)
+                }
+
+            });
+
+            ingredientsMap.forEach((ing, idx) => {
+                    console.log(ing)
+                    if (measureMap[idx]) finalIngredients[ing] = measureMap[idx]
+                    else finalIngredients[ing] = 'to taste'
+                })
+                // console.log(finalIngredients)
+            Object.entries(finalIngredients).forEach(([key, val], ind) => {
+                ingredients.textContent += `${key}: ${val}\n`
+            });
+        })
+});
