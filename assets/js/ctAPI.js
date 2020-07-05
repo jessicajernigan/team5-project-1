e1cae448ac941006147b90f1b05f758dbb7ee094
jeoldmixon <jeoldmixon@yahoo.com>
2020-07-05 13:05:28 -0500

    changes to ctAPI

--- assets/js/ctAPI.js
+++ assets/js/ctAPI.js
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-// ID Search box
-var searchInput = document.getElementById("cocktail-name-main") //This will need to be modified to match html page
-// ID Ingredients
-var ingredients = document.getElementById("ingredients") //This will need to be modified to match html page
-// ID Trivia
-var trivia = document.getElementById("trivia") //This will need to be modified to match html page
-// ID Drink Pic
-var drinkPic = document.getElementById("drinkPic") //This will need to be modified to match html page
-// ID Directions
-var directions = document.getElementById("directions") //This will need to be modified to match html page
-=======
 function fetchDrink() {
->>>>>>> 0b133b576128a73ea3a1a34f5a3a5e3b7c09a35d
 
     // ID Search box
     var searchInput = document.getElementById("drink-search").value //This will need to be modified to match html page
@@ -25,17 +12,6 @@ function fetchDrink() {
     var directions = document.getElementById("directions") //This will need to be modified to match html page
         // ADD BUTTON ID to drink-search
 
-<<<<<<< HEAD
-var populateSearchResults = function () {
-    var drinks = searchInput.value
-    console.log("drinks: ", searchInput.value);
-
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinks)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-=======
     var drinkDisplayed = searchInput.trim().replace(" ", "_");
     ingredients.textContent = "";
     console.log(searchInput);
@@ -45,7 +21,6 @@ var populateSearchResults = function () {
             return response.json();
         })
         .then(function(data) {
->>>>>>> 0b133b576128a73ea3a1a34f5a3a5e3b7c09a35d
             console.log(data)
             directions.innerHTML = data.drinks[0].strInstructions
             drinkPic.src = data.drinks[0].strDrinkThumb
@@ -63,19 +38,14 @@ var populateSearchResults = function () {
             });
 
             ingredientsMap.forEach((ing, idx) => {
-                // console.log(ing)
-                if (measureMap[idx]) finalIngredients[ing] = measureMap[idx]
-                else finalIngredients[ing] = 'to taste'
-            })
-            // console.log(finalIngredients)
-            Object.entries(finalIngredients).forEach(([key, val], ing) => {
+                    console.log(ing)
+                    if (measureMap[idx]) finalIngredients[ing] = measureMap[idx]
+                    else finalIngredients[ing] = 'to taste'
+                })
+                // console.log(finalIngredients)
+            Object.entries(finalIngredients).forEach(([key, val], ind) => {
                 ingredients.textContent += `${key}: ${val}\n`
             });
         })
 }
-<<<<<<< HEAD
-
-
-=======
 document.getElementById("search-button-drink").addEventListener("click", fetchDrink);
->>>>>>> 0b133b576128a73ea3a1a34f5a3a5e3b7c09a35d
