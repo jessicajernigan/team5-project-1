6717d7d94c3da52d6d9258ab2e1fc21fc01a8b3f
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-07-05 13:06:47 -0500

    remove references to old function

--- assets/js/storeSearches.js
+++ assets/js/storeSearches.js
@@ -23,7 +23,6 @@ function storeDrinkSearches(event) {
       event.preventDefault();
       var drink = $(this).attr("value");
       console.log("Here's 'drink' when I click a button: ", drink);
-      populateSearchResults(drink);
     }
 
     previousSearches.appendChild(newBtn);
@@ -46,7 +45,6 @@ function displayStoredDrinks() {
     newBtn.onclick = function (event) {
       var drink = event.target.textContent;
       drinkDisplayed.innerHTML = drink;
-      populateSearchResults();
       console.log("Here's the 'drink' value when I click a button that loaded on the page: ", drink);
     
     }
